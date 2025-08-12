af34d9ecaff0968f36d795911658d9e1b0088fa5
aperkinsabnormal <aperkins@abnormalsecurity.com>
2025-08-12 09:27:41 -0400

    Updated sfdc_formula_view.sql to avoid using outdated back for dbt_utils.get_column_values

--- macros/sfdc_formula_view.sql
+++ macros/sfdc_formula_view.sql
@@ -1,13 +1,11 @@
 {%- macro sfdc_formula_view(source_table, source_name='salesforce', materialization='view', using_quoted_identifiers=False, full_statement_version=true, reserved_table_name=none, fields_to_include=none) -%}
 
--- Best practice for this model is to be materialized as view. That is why we have set that here.
 {{
     config(
         materialized = materialization
     )
 }}
 
--- Raise a warning if users are trying to use full_statement_version=false. We are keeping the variable in the macro, however, since we don't want errors if they previously set it to true.
 {% if not full_statement_version %}
     {{ exceptions.warn("\nERROR: The full_statement_version=false, reserved_table_name, and fields_to_include parameters are no longer supported. Please update your " ~ this.identifier|upper ~ " model to remove these parameters.\n") }}
     See_full_model_error_in_log
@@ -15,15 +13,35 @@
 {% else %}
 
     {% if using_quoted_identifiers %}
-    {%- set table_results = dbt_utils.get_column_values(table=source(source_name, 'fivetran_formula_model'), 
-                                                        column='"MODEL"' if target.type in ('snowflake') else '"model"' if target.type in ('postgres', 'redshift', 'snowflake') else '`model`', 
-                                                        where=("\"OBJECT\" = '" if target.type in ('snowflake') else "\"object\" = '" if target.type in ('postgres', 'redshift') else "`object` = '") ~ source_table ~ "'") -%}
+        {%- set quoted_col = (
+            '"MODEL"' if target.type in ('snowflake') else
+            '"model"' if target.type in ('postgres', 'redshift', 'snowflake') else
+            '`model`'
+        ) -%}
+
+        {%- set quoted_where = (
+            "\"OBJECT\" = '" if target.type in ('snowflake') else
+            "\"object\" = '" if target.type in ('postgres', 'redshift') else
+            "`object` = '"
+        ) ~ source_table ~ "'" -%}
+
+        {%- set sql = dbt_utils.get_column_values_sql(
+            table=source(source_name, 'fivetran_formula_model'),
+            column=quoted_col,
+            where=quoted_where
+        ) -%}
 
     {% else %}
-    {%- set table_results = dbt_utils.get_column_values(table=source(source_name, 'fivetran_formula_model'), column='model', where="object = '" ~ source_table ~ "'") -%}
-
+        {%- set sql = dbt_utils.get_column_values_sql(
+            table=source(source_name, 'fivetran_formula_model'),
+            column='model',
+            where="object = '" ~ source_table ~ "'"
+        ) -%}
     {% endif %}
 
+    {% set table_results = run_query(sql).columns[0].values() %}
     {{ table_results[0] }}
+
 {% endif %}
+
 {%- endmacro -%}
